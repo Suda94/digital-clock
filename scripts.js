e0f82edee90e1df44e2b9d1f1f88e84c80ba1ac2
Suda94 <grg_sudarshan94@hotmail.com>
2023-01-08 03:38:11 +0000

    adding script

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
+// digital clock code:
 function showTime() {
     let date = new Date();
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
     let session = "AM";
-
 
     if (h == 0) {
         h = 12;
@@ -29,3 +29,54 @@ function showTime() {
     setTimeout(showTime, 1000);
 }
 showTime();
+
+
+// Analog Clock function:
+const deg = 6;
+const hours = document.querySelector(".hour");
+const mins = document.querySelector("min");
+const secs = document.querySelector(".sec");
+
+const setClock = () => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hours.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    mins.style.transform = `rotateZ(${mm}deg)`;
+    secs.style.transform = `rotateZ(${ss}deg)`;
+};
+
+//first time:
+setClock();
+
+//update every 1000ms
+setInterval(setClock, 1000);
+
+
+const themeSwitch = (evt) => {
+    const switchBtn = evt.target;
+    if (switchBtn.textContent.toLowerCase() === "light") {
+        switchBtn.textContent = "dark";
+        //localStorage.setItem("theme", "dark");
+
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+        switchBtn.textContent = "light";
+        //localStorage.setItem("theme", "light");//add this
+
+        document.documentElement.setAttribute("data-theme", "light");
+    }
+};
+
+const switchModeBtn =
+    document.querySelector(".switch-btn");
+switchModeBtn.addEventListener("click", switchTheme, false);
+
+let currentTheme = "dark";
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    switchModeBtn.textContent = currentTheme;
+}
